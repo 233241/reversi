d4a6473f5ea6125ad2821f7edf1c47ab859ca07b
m <233241ii@gmail.com>
2016-05-18 17:32:08 +0200

    ustawilem mavena i dodalem podstawowy zestaw paczek

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created on 18.05.16.
+ *
+ * @author m
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("HELLO");
+    }
+}
